84916e8d0dec5e0287e9d38f00ffe4fb893ed31e
mohammadshadan <shadan.m@gmail.com>
2016-08-23 04:03:13 +0400

    Updated run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,7 @@
+##FINAL PROJECT SUBMISSION FOR GETTING AND CLEANING DATA COURSE
+##WRITTEN BY : MOHAMMAD SHADAN
+##DATE       : 23-APR-2016
+
 #run_analysis.R does the following
 #1. Merges the training and the test sets to create one data set.
 #2. Extracts only the measurements on the mean and standard deviation for each measurement.
